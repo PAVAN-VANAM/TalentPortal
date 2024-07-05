b744937b4b9756ab067fa8eb2e432ee317bd840b
Bandari Naveen <bandarin29@gmail.com>
2024-07-05 13:42:35 +0530

    tailwind

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 function Login(){
     return(
         <>
+        <div className='bg-black text-white'>
           <h2>Login</h2>
+          </div>
         </>
     )
 }
