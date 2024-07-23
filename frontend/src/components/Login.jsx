316d00db3e364ca3d71f504b35077ba1c9d33a06
SREE LAKSHMI SREE PARASARA <121387626+S-P-Sreelakshmi630@users.noreply.github.com>
2024-07-23 15:26:30 +0530

    model.jsx(profile page)

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -89,8 +89,6 @@ const LoginPage = () => {
             </div>
         </div>
       </div>
-
-
     </>
   );
 };
--- frontend/src/components/model.jsx
+++ frontend/src/components/model.jsx
@@ -1,6 +1,8 @@
-//import React from 'react';
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
-import { FaBriefcase, FaMapMarkerAlt,FaGraduationCap, FaStar, FaStarHalf, FaSchool,FaTools } from 'react-icons/fa';
+import { FaBriefcase, FaMapMarkerAlt, FaGraduationCap, FaStar, FaStarHalf, FaSchool, FaTools } from 'react-icons/fa';
+import { LuShare } from "react-icons/lu";
+import { IoLocationOutline } from "react-icons/io5";
+import { MdWorkOutline } from "react-icons/md";
 
 // Function to render star rating
 const renderStars = (rating) => {
@@ -22,76 +24,65 @@ const renderStars = (rating) => {
 };
 
 // eslint-disable-next-line react/prop-types
-const Models = ({ name, rating, designation, address, education = [],techStack, workExperience=[], testimonials }) => {
+const Models = ({ name, rating, designation, address, education = [], techStack, workExperience = [], testimonials, videoUrl }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <>
       <Button onPress={onOpen} className="bg-gray-50 text-primary-600">
-          View Profile &gt;
+        View Profile &gt;
       </Button>
 
       <Modal className="absolute right-0 w-[900px] h-full bg-white rounded-none translate-x-7 " isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
-              <h1 className="flex gap-1 text-xl font-bold m-4">Profile Details</h1>
+              <h2 className="text-xl flex items-center font-bold"><span className="mr-2"><LuShare size={24} /></span>Share</h2>
               <ModalBody>
-                <div className="flex justify-around items-center text-xl mt-6">
-                  <img src="https://randomuser.me/api/portraits/women/2.jpg" className="rounded-full w-36" alt="Profile" />
-                  <div>
-                  <h4 className="text-2xl font-bold">{name}</h4>
-                  <h4 className="text-base flex gap-2 items-center text-gray-600"><span><FaBriefcase /></span>{designation}</h4>
-                  <h2 className="text-base flex items-center gap-2 text-gray-600"><span><FaMapMarkerAlt /></span>{address.city}</h2>
-                  <div className="flex items-center gap-2">
-                    {renderStars(rating)}
-                  </div>
-                  <button className='border border-cyan-800 text-lg rounded-xl shadow mt-4 p-1 flex flex-row justify-center item-center gap-3'>
-                  <img src="https://www.svgrepo.com/show/75820/linkedin.svg" alt="linkedin image" className='pt-1 flex flex-row justify-center item-center w-[24px]' />
-                  <div className='pt-1 flex flex-row justify-center items-center'>LinkedIn</div>
-                  </button>
-                  </div>
-                </div>
-                <hr className="mt-5 border-1" />
-                <div className="mt-2">
-                  <h4 className="text-lg font-semibold">Education</h4>
-                  {education.length > 0 ? (
-                    education.map((edu, index) => (
-                      <div key={index} className="text-xl flex gap-2 justify-start h-12 items-center  text-gray-900">
-                        <span ><FaSchool /></span>{edu.instituteName}  - <span><FaGraduationCap/></span> {edu.courseName}
-                        
-                      </div>
-                    ))
-                  ) : (
-                    <p className="text-xs text-gray-600">No education details available</p>
-                  )}
-                </div>
-                <div className="mt-2">
-                  <h4 className="text-md font-semibold">Work Experience</h4>
-                  {workExperience.length > 0 ? (
-                    workExperience.map((work, index) => (
-                      <div key={index} className="text-xs flex gap-2 items-center text-gray-600">
-                        <span><FaBriefcase /></span>{work.companyName} - {work.designation}
-                      </div>
-                    ))
-                  ) : (
-                    <p className="text-xs text-gray-600">No work experience details available</p>
-                  )}
-                </div>
-                <div className="mt-2">
-                  <h4 className="text-md font-semibold">Tech Stack</h4>
-                  {techStack.length > 0 ? (
-                    techStack.map((tech, index) => (
-                      <div key={index} className="text-xs flex gap-2 items-center text-gray-600">
-                        <span><FaTools /></span>{tech}
-                      </div>
-                    ))
-                  ) : (
-                    <p className="text-xs text-gray-600">No tech stack details available</p>
-                  )}
-                </div>
-               
-              </ModalBody>
+  <div className="flex justify-around items-center text-xl mt-6">
+    <img src="https://randomuser.me/api/portraits/women/2.jpg" className="rounded-full w-36" alt="Profile" />
+    <div>
+      <h4 className="text-2xl font-bold">{name}</h4>
+      <h4 className="text-base flex gap-2 items-center text-gray-600">{designation}</h4>
+      <h4 className="text-base flex gap-2 items-center text-gray-600">Software Engineer at Arcesium</h4>
+      <button className='border border-cyan-800 text-lg rounded-xl shadow mt-4 p-1 flex flex-row justify-center item-center gap-3'>
+        <img src="https://www.svgrepo.com/show/75820/linkedin.svg" alt="linkedin image" className='pt-1 flex flex-row justify-center item-center w-[24px] border-blue-600' />
+        <div className='pt-1 flex flex-row justify-center items-center text-blue-600'>LinkedIn</div>
+      </button>
+    </div>
+  </div>
+  <div className="flex mt-4">
+    <h3>User rating : </h3>{renderStars(rating)}
+  </div>
+
+  <div className="mt-2">
+    <h2 className="text-base flex items-center gap-2 text-gray-600"><span><IoLocationOutline /></span>{address.city}</h2>
+    {education.length > 0 ? (
+      education.map((edu, index) => (
+        <div key={index} className="text-base flex gap-2 justify-start h-12 items-center text-gray-600">
+          <span><FaSchool /></span>{edu.instituteName} <span><FaGraduationCap /></span> {edu.courseName}
+        </div>
+      ))
+    ) : (
+      <p className="text-xs text-gray-600">No education details available</p>
+    )}
+    {workExperience.length > 0 ? (
+      workExperience.map((work, index) => (
+        <div key={index} className="text-xs flex gap-2 items-center text-gray-600">
+          <span><MdWorkOutline /></span>{work.companyName} - {work.designation}
+        </div>
+      ))
+    ) : (
+      <p className="text-base text-gray-600">No work experience details available</p>
+    )}
+  </div>
+
+  <div className="mt-4">
+    <h1 className="font-bold text-xl text-gray-600">Reviews by Manideep Siva on YouTube</h1><br></br>
+    <iframe className="rounded-medium" width="400" height="210"  src="https://www.youtube.com/embed/LEk6ieSO3jE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+  </div>
+</ModalBody>
+
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Close
